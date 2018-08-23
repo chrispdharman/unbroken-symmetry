99e0fed135ff9d83e5ad4814328127a770e1353a
chrispdharman <christopher.p.d.harman@gmail.com>
2018-08-23 18:56:29 +0100

    Welcome page animation complete

--- scripts/welcomeAnimation.js
+++ scripts/welcomeAnimation.js
@@ -1,7 +1,22 @@
+// get current position
+var col = 0;
+var delay = 500;
+
 /* Define the function */
-function codeAddress() {
-  alert("It's alive!");
+function animateMenu() {
+  var colMax = 4;
+  if (col++ < colMax) {
+    /* Debug lines */
+    //var date = new Date;
+    //console.log("Column ",col,", Time: ",date.getTime())
+
+    /* update the img element with arrow.png */
+    document.getElementById('anim_'+col.toString()).src = 'images/arrow.png';
+    /* wait delay (in ms) to recursively run for next column*/
+    setTimeout(animateMenu, delay);
+  }
 }
 
+//animateMenu()
 /* Execute the function on page loading */
-window.onload = codeAddress;
+window.onload = setTimeout(animateMenu, 500);
