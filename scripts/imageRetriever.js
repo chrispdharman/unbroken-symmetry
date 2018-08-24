97f8e1d77f150f636f609630c0e19e35208915d3
chrispdharman <christopher.p.d.harman@gmail.com>
2018-08-24 13:54:04 +0100

    Added input image placeholder box

---
+++ scripts/imageRetriever.js
@@ -0,0 +1,4 @@
+// get current position
+var inputImage = document.getElementById('inputImg');
+console.log('Input image=',inputImage);
+document.getElementById('inputImgDisplay').src = 'images/arrow.png';
