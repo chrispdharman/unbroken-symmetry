f55983181d038516eac7ebf4fb2dcf91e75399e2
chrispdharman <christopher.p.d.harman@gmail.com>
2018-09-06 11:29:47 +0100

    Successful image displayer

--- scripts/imageRetriever.js
+++ scripts/imageRetriever.js
@@ -1,6 +1,11 @@
 /* Function to update any uploaded input image */
-function updateImage(arg) {
-	var image = document.getElementById('inputImgDisplay');
-	console.log(image);
-	image.src = URL.createObjectURL(arg.target.files[0]);
+function updateImage() {
+  /* Identify html element */
+  var imgSrc = document.getElementById("inputImg").files[0];
+  var image = document.getElementById("inputImgDisplay");
+  console.log("imgSrc value=",imgSrc);
+
+  /* Display uploaded image in html */
+	image.src = window.URL.createObjectURL(imgSrc);
+  return false;
 };
